44d3e213ed3581c7564f94cec3f9beced08ea5b4
MohamedAwad5 <mohamedmedo331155@gmail.com>
2024-12-08 02:20:26 +0200

    generate jwt

---
+++ src/main/java/com/ecommerce/ecommerce_backend/service/JWTService.java
@@ -0,0 +1,40 @@
+package com.ecommerce.ecommerce_backend.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.ecommerce.ecommerce_backend.model.LocalUser;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JWTService {
+
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
+
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    @Value("${jwt.expiryInSeconds}")
+    private Long expiryInSeconds;
+
+    private Algorithm algorithm;
+
+    @PostConstruct // executed once after the bean’s initialization is complete but before beans is used by other parts of the application
+    public void postConstruct(){
+        algorithm = Algorithm.HMAC256(algorithmKey);
+    }
+
+    private static final String USERNAME_KEY = "USERNAME";
+
+    public String generateToken(LocalUser user){
+        return JWT.create()
+                .withClaim(USERNAME_KEY, user.getUsername())
+                .withIssuer(issuer)
+                .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * expiryInSeconds)))
+                .sign(algorithm);
+    }
+}
