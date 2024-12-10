f5aaec842401853429ea3e903d33110e84d025b4
MohamedAwad5 <mohamedmedo331155@gmail.com>
2024-12-11 00:01:54 +0200

    change package name

--- src/main/java/com/ecommerce/ecommerce_backend/security/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/ecommerce_backend/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.ecommerce.ecommerce_backend.config;
+package com.ecommerce.ecommerce_backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
